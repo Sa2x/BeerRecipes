0951e2c39795fb9bb289528f978d6a2775eb6172
Sa2x <bsanyi1998@gmail.com>
2021-04-11 21:01:01 +0200

    Defined beers interactors and modules

--- app/src/main/java/com/example/beerrecipes/ui/interactor/InteractorModule.kt
+++ app/src/main/java/com/example/beerrecipes/ui/interactor/InteractorModule.kt
@@ -1,2 +1,15 @@
 package com.example.beerrecipes.ui.interactor
 
+import com.example.beerrecipes.ui.MainActivity
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
+
+@InstallIn(MainActivity::class)
+@Module
+class InteractorModule {
+    @Provides
+    @Singleton
+    fun provideBeersInteractor(){}
+}
--- app/src/main/java/com/example/beerrecipes/ui/interactor/beers/BeersInteractor.kt
+++ app/src/main/java/com/example/beerrecipes/ui/interactor/beers/BeersInteractor.kt
@@ -1,4 +1,23 @@
 package com.example.beerrecipes.ui.interactor.beers
 
-class BeersInteractor {
+import com.example.beerrecipes.ui.model.Beer
+import javax.inject.Inject
+
+class BeersInteractor @Inject constructor() {
+
+    fun getBeers(){
+
+    }
+
+    fun searchBeers(BeerQuery: String){
+
+    }
+
+    fun addBeerToFavourite(beer : Beer){
+
+    }
+
+    fun removeBeerToFavourite(beer:Beer){
+
+    }
 }
