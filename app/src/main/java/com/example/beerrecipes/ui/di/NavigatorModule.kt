abfbb239756137a21a2453d2a67de1b5a5678b15
Sa2x <bsanyi1998@gmail.com>
2021-04-11 20:40:51 +0200

    Defined modules and navigator components

--- app/src/main/java/com/example/beerrecipes/ui/di/NavigatorModule.kt
+++ app/src/main/java/com/example/beerrecipes/ui/di/NavigatorModule.kt
@@ -1,2 +1,17 @@
 package com.example.beerrecipes.ui.di
 
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import com.example.beerrecipes.ui.navigator.AppNavigator
+import com.example.beerrecipes.ui.navigator.AppNavigatorImpl
+
+@InstallIn(ActivityComponent::class)
+@Module
+abstract class NavigatorModule {
+
+    @Binds
+    abstract fun bindNavigator(impl: AppNavigatorImpl): AppNavigator
+
+}
